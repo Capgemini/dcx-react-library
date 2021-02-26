1596ff77927a4d43fd35c9042136ca5184bb7182
Daniele Zurico <danielezurico@danieles-mbp.mynet>
2021-02-26 12:59:52 +0000

    add styled components

--- src/example/example.tsx
+++ src/example/example.tsx
@@ -1,9 +1,20 @@
 import React, { FC, HTMLAttributes, ReactChild } from 'react';
 import styles from './example.module.css';
+import styled from 'styled-components'
 
 export interface Props extends HTMLAttributes<HTMLDivElement> {
   children?: ReactChild;
 }
 
-export const Example: FC<Props> = ({ children }) => 
- <div className={styles.daniele} data-testid="example">{children || `I'm just a demo component`}</div>
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
+
+export const Example: FC<Props> = ({ children }) => (
+  <div className={styles.daniele} data-testid="example">{children || `I'm just a demo component`}
+    <Title>Styled component</Title>
+  </div>
+)
+
