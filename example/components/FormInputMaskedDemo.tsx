8492bb8442fb5b38343316b91dbcd6636e6b1fb0
Daniele Zurico <daniele.zurico@gmail.com>
2021-03-06 18:02:40 +0000

    fix comment

--- example/components/FormInputMaskedDemo.tsx
+++ example/components/FormInputMaskedDemo.tsx
@@ -2,27 +2,30 @@ import * as React from 'react';
 import { FormInputMasked } from 'dcx-react-library';
 export const FormInputMaskedDemo = () => {
   const [value, setValue] = React.useState('');
+  const [typedValue, setTypedValue] = React.useState('');
   const handleChange = evt => {
-    console.log('value', evt.value);
-    console.log('unmaskedValue', evt.unmaskedValue);
+    setTypedValue(evt.unmaskedValue);
     setValue(evt.value);
   };
   return (
-    <FormInputMasked
-      options={{
-        mask: '£num',
-        blocks: {
-          num: {
-            // nested masks are available!
-            mask: Number,
-            thousandsSeparator: ' ',
+    <>
+      <FormInputMasked
+        options={{
+          mask: '£num',
+          blocks: {
+            num: {
+              // nested masks are available!
+              mask: Number,
+              thousandsSeparator: ' ',
+            },
           },
-        },
-      }}
-      value={value}
-      onChange={handleChange}
-      type="text"
-      name="daniele"
-    />
+        }}
+        value={value}
+        onChange={handleChange}
+        type="text"
+        name="daniele"
+      />
+      <span>Typed value: {typedValue}</span>
+    </>
   );
 };
