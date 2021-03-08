ad5178309ab46a0c0a98edfc96edc457e5d7e31b
Isaac Babalola <info@isaacbabalola.co.uk>
2021-03-08 15:38:18 +0000

    fix: formInputMaskDemo breaking app

--- example/index.tsx
+++ example/index.tsx
@@ -2,12 +2,13 @@ import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { FormInputDemo } from './components/FormInputDemo';
-import { FormInputMaskedDemo } from './components/FormInputMaskedDemo';
-const App = () => (
-  <div>
-    <FormInputDemo />
-    {/* <FormInputMaskedDemo /> */}
-  </div>
-);
+// import { FormInputMaskedDemo } from './components/FormInputMaskedDemo';
+const App = () => {
+  return (
+    <div>
+      <FormInputDemo />
+    </div>
+  );
+};
 
 ReactDOM.render(<App />, document.getElementById('root'));
