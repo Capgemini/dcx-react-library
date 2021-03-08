1cf4f0226d4e04a04d29b2a159e64e8515fd9844
Isaac Babalola <info@isaacbabalola.co.uk>
2021-03-08 20:01:51 +0000

    chore: clean up eslint changes

--- example/index.tsx
+++ example/index.tsx
@@ -2,12 +2,12 @@ import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { FormInputDemo } from './components/FormInputDemo';
-// import { FormInputMaskedDemo } from './components/FormInputMaskedDemo';
+import { FormInputMaskedDemo } from './components/FormInputMaskedDemo';
 const App = () => (
   <div>
     <FormInputDemo />
+    <FormInputMaskedDemo />
   </div>
 );
-
 
 ReactDOM.render(<App />, document.getElementById('root'));
