1463714a629625ff5dd7fb1302adcec482e29391
Isaac Babalola <info@isaacbabalola.co.uk>
2021-03-14 11:28:30 +0000

    refactor: renaming test file name to match functional component file name

---
+++ src/formRadio/__test__/FormRadio.test.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import fireEvent from '@testing-library/user-event';
+import '@testing-library/jest-dom';
+import { FormRadio } from '../FormRadio';
+
+describe('FormRadio', () => {
+  it('should display the formRadio content', () => {
+    const handleChange = jest.fn();
+
+    render(
+      <FormRadio
+        id="myId"
+        name="group1"
+        value="choice 1"
+        label="my label"
+        onChange={handleChange}
+      />
+    );
+
+    expect(screen.getByRole('form-radio')).toBeInTheDocument();
+    expect(screen.getByLabelText('my label').getAttribute('value')).toBe(
+      'choice 1'
+    );
+  });
+
+  it('should call on change', () => {
+    const handleChange = jest.fn();
+
+    render(
+      <FormRadio
+        id="myId"
+        name="group1"
+        value="choice 1"
+        label="my label"
+        onChange={handleChange}
+      />
+    );
+
+    fireEvent['click'](screen.getByLabelText('my label'));
+
+    expect(handleChange).toHaveBeenCalled();
+  });
+
+  it('should not call on change if undefined', () => {
+    const handleChange = jest.fn();
+
+    render(
+      <FormRadio id="myId" name="group1" value="choice 1" label="my label" />
+    );
+
+    fireEvent['click'](screen.getByLabelText('my label'));
+
+    expect(handleChange).not.toHaveBeenCalled();
+  });
+});
