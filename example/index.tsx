ee11b94393ce4deb3431a2337aa8d4146bc04e11
Ivina Pontes <ivina.pontes@capgemini.com>
2021-03-23 11:58:03 +0000

    feat: initial router implementation

--- example/index.tsx
+++ example/index.tsx
@@ -1,20 +1,44 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-
+import { BrowserRouter, Route, Link, Switch } from 'react-router-dom';
 import {
   FormInputDemo,
   FormInputMaskedDemo,
   AutocompleteDemo,
   FormRadioDemo,
 } from './components';
-
+import { Login } from './pages/Login';
 const App = () => (
   <div>
-    <FormInputDemo />
-    <FormInputMaskedDemo />
-    <FormRadioDemo />
-    <AutocompleteDemo />
+    <BrowserRouter>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">FormInputDemo</Link>
+          </li>
+          <li>
+            <Link to="/login">Login Example</Link>
+          </li>
+          <li>
+            <Link to="/masked">masked</Link>
+          </li>
+          <li>
+            <Link to="/radio">radio</Link>
+          </li>
+          <li>
+            <Link to="/AutocompleteDemo">AutocompleteDemo</Link>
+          </li>
+        </ul>
+      </nav>
+      <Switch>
+        <Route path="/" exact component={FormInputDemo} />
+        <Route path="/login" component={Login} />
+        <Route path="/masked" component={FormInputMaskedDemo} />
+        <Route path="/radio" exact component={FormRadioDemo} />
+        <Route path="/AutocompleteDemo" component={AutocompleteDemo} />
+      </Switch>
+    </BrowserRouter>
   </div>
 );
 
---
+++ example/pages/HomePage.tsx
@@ -0,0 +1,11 @@
+import 'react-app-polyfill/ie11';
+import * as React from 'react';
+
+import { AutocompleteDemo } from '../components';
+
+export const Home = () => (
+  <>
+    <h1>Home</h1>
+    <AutocompleteDemo />
+  </>
+);
