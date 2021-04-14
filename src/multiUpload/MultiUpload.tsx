3433bfb74fc9bdc68a4a26849bb9e7d4845180c6
Isaac Babalola <info@isaacbabalola.co.uk>
2021-04-14 08:03:15 +0100

    feat: adding multiupload component

---
+++ src/multiUpload/MultiUpload.tsx
@@ -0,0 +1,89 @@
+import React from 'react';
+import {
+  ErrorMessage,
+  ErrorMessageProps,
+  Hint,
+  HintProps,
+  Roles,
+} from '../common';
+
+type MultiUploadProps = {
+  /**
+   * multi upload name
+   */
+  name: string;
+  /**
+   * multi upload comma-separated list of file types accepted by the component
+   */
+  acceptedFormats?: string;
+  /**
+   * multi upload whether or not to allow directories and files both to be selected in the file list
+   */
+  allowDirectories?: string;
+  /**
+   * multi upload aria label
+   */
+  ariaLabel?: string;
+  /**
+   * multi upload class name
+   */
+  className?: string;
+  /**
+   * multi upload error
+   */
+  error?: ErrorMessageProps;
+  /**
+   * multi upload hint
+   */
+  hint?: HintProps;
+  /**
+   * multi upload id
+   */
+  id?: string;
+  /**
+   * multi upload input properties
+   */
+  inputProperties?: any;
+  /**
+   * multi upload label
+   */
+  label?: string;
+  /**
+   * multi upload label properties for customisation
+   */
+  labelProperties?: any;
+};
+
+export const MultiUpload = ({
+  name,
+  acceptedFormats,
+  allowDirectories,
+  ariaLabel,
+  className,
+  error,
+  hint,
+  id,
+  inputProperties,
+  label,
+  labelProperties,
+}: MultiUploadProps) => (
+  <div className={className}>
+    {label && (
+      <label {...labelProperties} htmlFor={id}>
+        {label}
+      </label>
+    )}
+    {hint && <Hint {...hint} />}
+    {error && <ErrorMessage {...error} />}
+    <input
+      {...inputProperties}
+      id={id}
+      type="file"
+      role={Roles.button}
+      name={name}
+      aria-label={ariaLabel || name}
+      accept={acceptedFormats}
+      allowdirs={allowDirectories}
+    />
+  </div>
+);
