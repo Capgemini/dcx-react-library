90040a237bd814dea4cda2c18d8855826b86112f
Isaac Babalola <info@isaacbabalola.co.uk>
2021-05-04 18:20:56 +0100

    fix: resolving console warning found in storybook

--- src/common/components/index.ts
+++ src/common/components/index.ts
@@ -2,4 +2,4 @@ export { ErrorMessage } from './ErrorMessage';
 export { Hint } from './Hint';
 export { Legend } from './Legend';
 export { OptionGroup } from './OptionGroup';
-export { Option, OptionProps } from './Option';
+export { Option } from './Option';
