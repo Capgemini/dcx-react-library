826875b217b14ff3c3221386b0c95721898d59b0
Ivina Pontes <ivina.pontes@capgemini.com>
2021-05-09 22:39:05 +0100

    feat: no bullet and carets implementation added

--- example/components/SideNavBar.tsx
+++ example/components/SideNavBar.tsx
@@ -6,18 +6,39 @@ const SideNavItems = ({ items }) =>
   items.map(item => {
     const { link, title } = item;
     return (
-      <li key={title}>
-        <Link href={link}>{title}</Link>
+      <li className="no-bullets" key={title}>
+        <Link className="no-bullets" href={link}>
+          {title}
+        </Link>
       </li>
     );
   });
 
+const AnotherComponent = ({ section }) => {
+  const [open, setOpen] = React.useState(false);
+
+  const caretClass = `caret ${open ? 'caret-up' : ''}`;
+  return (
+    //align it to left
+    <div>
+      <button
+        onClick={() => {
+          setOpen(!open);
+        }}
+      >
+        {' '}
+        {section.title} <span className={caretClass}> </span>
+      </button>
+      <ul style={{ display: open ? 'contents' : 'none' }}>
+        <SideNavItems items={section.items} />
+      </ul>
+    </div>
+  );
+};
+
 export const SideNavBar = () => {
-  const navBarItems = data.map(section => (
-    <ul key={section.title}>
-      {section.title}
-      <SideNavItems items={section.items} />
-    </ul>
+  const navBarItems = data.map((section: any) => (
+    <AnotherComponent section={section} key={section.title} />
   ));
   return <div>{navBarItems}</div>;
 };
