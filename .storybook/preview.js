d5a1213f0c51bb3138be216e5f41a2cd7d592348
Alex Webber <alexwbbr@gmail.com>
2021-05-10 12:26:48 +0100

    feat: updated storybook sorting and added detail on stories

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,4 +2,15 @@
 export const parameters = {
   // https://storybook.js.org/docs/react/essentials/actions#automatically-matching-args
   actions: { argTypesRegex: '^on.*' },
+  options: {
+    storySort: (a, b) => {
+      if (a[0].includes('dcxlibrary-form')) {
+        return 2000;
+      } else if (a[0].includes('progress')) {
+        return 1000;
+      } else {
+        return 0;
+      }
+    }
+  }
 };
