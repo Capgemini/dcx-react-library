3bce49543be485a9b8630e5d2340488da30bbe46
Isaac Babalola <info@isaacbabalola.co.uk>
2021-05-16 12:23:09 +0100

    fix: tabGroup live tab import

--- stories/liveEdit/TabGroupLive.tsx
+++ stories/liveEdit/TabGroupLive.tsx
@@ -8,14 +8,13 @@ const TabGroupDemo = `
 function TabGroupDemo() {
     return (
       <TabGroup
-        defaultActiveTab="Tab 2"
         activeTabClassName="tab-list-active"
         disabledClassName="tab-list-disabled"
         tabClassName="tab-list-item"
         className="tab-list"
       >
         <Tab label="Tab 1">This is content for <em>tab 1</em></Tab>
-        <Tab label="Tab 2">This is content for <em>tab 2</em></Tab>
+        <Tab label="Tab 2" activeTab="Tab 2">This is content for <em>tab 2</em></Tab>
         <Tab label="Tab 3">This is content for <em>tab 3</em></Tab>
         <Tab label="Tab 4" disabled={true}>This is content for <em>tab 4</em></Tab>
       </TabGroup>
@@ -24,16 +23,16 @@ function TabGroupDemo() {
 `.trim();
 
 const TabGroupDemoLive = () => {
-    const scope = { TabGroup, Tab };
-    return (
-        <LiveProvider code={TabGroupDemo} scope={scope}>
-        <div className="container">
-          <LiveEditor className="liveEditor" aria-label="editor" />
-          <LivePreview className="livePreview" aria-label="preview" />
-        </div>
-        <LiveError className="liveError" aria-label="error" />
-      </LiveProvider>
-    )
-}
+  const scope = { TabGroup, Tab };
+  return (
+    <LiveProvider code={TabGroupDemo} scope={scope}>
+      <div className="container">
+        <LiveEditor className="liveEditor" aria-label="editor" />
+        <LivePreview className="livePreview" aria-label="preview" />
+      </div>
+      <LiveError className="liveError" aria-label="error" />
+    </LiveProvider>
+  );
+};
 
 export default TabGroupDemoLive;
