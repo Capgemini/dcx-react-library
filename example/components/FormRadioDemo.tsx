457ecb1c3ea94d50f5de5b39c68d75b9144d8e5d
Isaac Babalola <info@isaacbabalola.co.uk>
2021-05-20 22:59:41 +0100

    feat: updating Form Radio Demo to showcase conditional input

--- example/components/FormRadioDemo.tsx
+++ example/components/FormRadioDemo.tsx
@@ -2,48 +2,128 @@ import * as React from 'react';
 import { FormRadio, FormGroup } from 'dcx-react-library';
 
 export const FormRadioDemo = () => {
-  const [value, setValue] = React.useState('');
+  const [selected, setSelected] = React.useState('single-2');
   const handleChange: React.ChangeEventHandler<HTMLInputElement> = event => {
-    setValue(event.currentTarget.value);
+    setSelected(event.currentTarget.value);
   };
+
+  const [conditionalSelected, setConditionalSelected] = React.useState(
+    'single-1'
+  );
+  const handleConditionalChange: React.ChangeEventHandler<HTMLInputElement> = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => setConditionalSelected(event.currentTarget.value);
+
+  const [inputFieldValues, setInputFieldValues] = React.useState({
+    'single-input-1': '',
+    'single-input-2': '',
+    'single-input-3': '',
+  });
+  const handleInputFieldChange: React.ChangeEventHandler<HTMLInputElement> = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setInputFieldValues({
+      ...inputFieldValues,
+      [event.currentTarget.name]: event.currentTarget.value,
+    });
+  };
+
+  React.useEffect(() => {
+    const output = document.getElementById('conditional-inputs');
+    if (output)
+      output.innerHTML = `${conditionalSelected} ${JSON.stringify(
+        inputFieldValues
+      )}`;
+  }, [inputFieldValues, conditionalSelected]);
 
   return (
     <>
-      <h1>Basic Single Radio Buttons</h1>
+      <h2>Basic Single Radio Buttons</h2>
       <FormRadio
         id="single-1"
-        value={value}
+        value="single-1"
+        selected={selected === 'single-1'}
         label="Single Radio 1 label text"
         name="group1"
-        inputProps={{
-          onChange: handleChange,
+        onChange={handleChange}
+      />
+      <FormRadio
+        id="single-2"
+        value="single-2"
+        selected={selected === 'single-2'}
+        label="Single Radio 2 label text"
+        name="group1"
+        onChange={handleChange}
+      />
+      <FormRadio
+        id="single-3"
+        value="single-3"
+        selected={selected === 'single-3'}
+        label="Single Radio 2 label text"
+        name="group1"
+        onChange={handleChange}
+      />
+      <br />
+      <h2>Basic Single Radio Buttons with conditional input</h2>
+      <div id="conditional-inputs" style={{ fontWeight: 'bold' }}></div>
+      <FormRadio
+        id="single-1"
+        value="single-1"
+        selected={conditionalSelected === 'single-1'}
+        label="Single Radio 1 label text"
+        name="group2"
+        onChange={handleConditionalChange}
+        conditional={{
+          name: 'single-input-1',
+          label: 'single input 1',
+          type: 'text',
+          value: inputFieldValues['single-input-1'],
+          onChange: handleInputFieldChange,
         }}
       />
       <FormRadio
         id="single-2"
-        value={value}
+        value="single-2"
+        selected={conditionalSelected === 'single-2'}
         label="Single Radio 2 label text"
-        name="group1"
-        inputProps={{
-          onChange: handleChange,
+        name="group2"
+        onChange={handleConditionalChange}
+        conditional={{
+          name: 'single-input-2',
+          label: 'single input 2',
+          type: 'text',
+          value: inputFieldValues['single-input-2'],
+          onChange: handleInputFieldChange,
         }}
       />
-      <br />
-      <FormGroup
+      <FormRadio
+        id="single-3"
+        value="single-3"
+        selected={conditionalSelected === 'single-3'}
+        label="Single Radio 2 label text"
         name="group2"
+        onChange={handleConditionalChange}
+        conditional={{
+          name: 'single-input-3',
+          label: 'single input 3',
+          type: 'text',
+          value: inputFieldValues['single-input-3'],
+          onChange: handleInputFieldChange,
+        }}
+      />
+      <FormGroup
+        name="group3"
         type="radio"
         items={[
           {
             id: 'radio-1',
             label: 'Option 1',
             value: 'value-1',
-            onChange: handleChange,
           },
           {
             id: 'radio-2',
             label: 'Option 2',
             value: 'value-2',
-            onChange: handleChange,
           },
           {
             id: 'radio-3',
@@ -53,7 +133,6 @@ export const FormRadioDemo = () => {
               text: 'Hint text for this item',
             },
             value: 'value-3',
-            onChange: handleChange,
           },
         ]}
         hint={{
