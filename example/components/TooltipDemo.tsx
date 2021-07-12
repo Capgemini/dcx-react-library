8feb413b1754f5084ec1c9e638ade243d123c724
Niku Sharma <niku.sharma@capgemini.com>
2021-07-12 09:55:35 +0100

    fixed story build issues

--- example/components/TooltipDemo.tsx
+++ example/components/TooltipDemo.tsx
@@ -1,38 +1,57 @@
 import React from 'react';
-import { Tooltip } from './toolTip';
+import { ToolTip } from 'dcx-react-library';
+//import { ToolTip } from './toolTip';
 
 export const TooltipDemo = () => {
-  const style = {
-    color: 'white',
-    background: 'red',
-    width: '250px',
-  };
-
   const content =
     "Created by British designer Thomas Heatherwick, it is hoped the Airo will go into production in China in 2023, with plans to make a million of them.The radical design is intended to address not only the pollution issue, but also help solve the space crisis.Critics are not convinced it can ever be more than a concept car.Despite designing London's new version of the iconic Routemaster bus, Mr Heatherwick is better known for architectural projects such as Google's headquarters in California and London.He told the BBC that while he had never designed a car before, he was intrigued by the brief.";
 
   return (
     <div style={{ padding: '250px' }}>
       <div>
-        <Tooltip content={content} direction="bottom" style={style}>
+        <ToolTip
+          content={content}
+          width="250px"
+          background="red"
+          color="white"
+          direction="bottom"
+        >
           <b> Testing Bottom </b>
-        </Tooltip>
+        </ToolTip>
       </div>
-    
+
       <div>
-        <Tooltip content={content} direction="top" style={style}>
+        <ToolTip
+          content={content}
+          direction="top"
+          width="250px"
+          background="red"
+          color="white"
+        >
           <b> Testing Top </b>
-        </Tooltip>
+        </ToolTip>
       </div>
       <div>
-        <Tooltip content={content} direction="left" style={style}>
+        <ToolTip
+          content={content}
+          direction="left"
+          width="250px"
+          background="red"
+          color="white"
+        >
           <b> Test Left </b>
-        </Tooltip>
+        </ToolTip>
       </div>
       <div>
-        <Tooltip content={content} direction="right" style={style}>
+        <ToolTip
+          content={content}
+          direction="right"
+          width="250px"
+          background="red"
+          color="white"
+        >
           <b> Test Right</b>
-        </Tooltip>
+        </ToolTip>
       </div>
     </div>
   );
