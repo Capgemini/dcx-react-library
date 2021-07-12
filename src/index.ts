d27aa55a578ae59f7d7967b0fc046299a5827189
Niku Sharma <niku.sharma@capgemini.com>
2021-07-12 09:55:35 +0100

    fixed story build issues

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ export * from './autocomplete/Autocomplete';
 export * from './button';
 export * from './progress';
 export * from './toggle';
+export * from './toolTip';
 export * from './formSelect';
 export * from './multiUpload';
 export * from './multiSelect';
