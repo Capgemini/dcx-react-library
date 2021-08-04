1a1a1f7a9d65b58489906a29ac0b449f7ba73143
Shefik <caner.shefik@capgemini.com>
2021-08-04 14:56:56 +0100

    test: {{ Tests for Range and component updates }}

--- example/components/RangeDemo.tsx
+++ example/components/RangeDemo.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {Range} from 'dcx-react-library';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faVolumeDown, faVolumeUp} from "@fortawesome/free-solid-svg-icons";
+import './range.scss';
 
 export const RangeDemo = () => {
     const [displayValue, setDisplayValue] = React.useState('');
@@ -12,10 +13,12 @@ export const RangeDemo = () => {
         <Range/>
         <h1>Range with custom values</h1>
         <Range min={0} max={100} value={75}/>
+        <h1>Range with custom styling</h1>
+        <Range inputClass="customRange"/>
         <h1>Disabled Range</h1>
         <Range disabled={true}/>
         <h1>Range with displayed value</h1>
-        <Range getAriaValueText={setDisplayValue} />
+        <Range onChange={setDisplayValue} />
         <h2>{displayValue}</h2>
         <h1>Range with prefix & suffix</h1>
         <Range
