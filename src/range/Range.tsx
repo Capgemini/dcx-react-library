7d14b2212410d687720533f521f9bf51336fc9c0
Isaac Babalola <info@isaacbabalola.co.uk>
2021-09-23 15:02:27 +0100

    fix: lint errors

--- src/range/Range.tsx
+++ src/range/Range.tsx
@@ -1,85 +1,79 @@
 import React from 'react';
 
 type RangeProps = {
-    /**
-     * max value of the range component
-     */
-    max?: number;
-    /**
-     * min value of the range component
-     */
-    min?: number;
-    /**
-     * current value of the range component
-     */
-    value?: number;
-    /**
-     * generic parameter to pass whatever element before the input
-     **/
-    prefix?: JSX.Element;
-    /**
-     * generic parameter to pass whatever element after the input
-     **/
-    suffix?: JSX.Element;
-    /**
-     * allow to enable disable the range slider
-     */
-    disabled?: boolean;
-    /**
-     * define the aria-label for accessibility. If no value is passed it will render input-slider
-     */
-    ariaLabel?: string;
-    /**
-     * function that will trigger all the time there's a change in the input
-     **/
-    onChange?: any;
-    /**
-     * optional className to style the slider
-     */
-    inputClass?: string;
-
+  /**
+   * max value of the range component
+   */
+  max?: number;
+  /**
+   * min value of the range component
+   */
+  min?: number;
+  /**
+   * current value of the range component
+   */
+  value?: number;
+  /**
+   * generic parameter to pass whatever element before the input
+   **/
+  prefix?: JSX.Element;
+  /**
+   * generic parameter to pass whatever element after the input
+   **/
+  suffix?: JSX.Element;
+  /**
+   * allow to enable disable the range slider
+   */
+  disabled?: boolean;
+  /**
+   * define the aria-label for accessibility. If no value is passed it will render input-slider
+   */
+  ariaLabel?: string;
+  /**
+   * function that will trigger all the time there's a change in the input
+   **/
+  onChange?: any;
+  /**
+   * optional className to style the slider
+   */
+  inputClass?: string;
 };
 
 export const Range = ({
-                          min,
-                          max,
-                          value,
-                          prefix,
-                          suffix,
-                          disabled=false,
-                          ariaLabel,
-                          onChange,
-                          inputClass,
-                          ...props
-                      }: RangeProps) => {
+  min,
+  max,
+  value,
+  prefix,
+  suffix,
+  disabled = false,
+  ariaLabel,
+  onChange,
+  inputClass,
+  ...props
+}: RangeProps) => {
+  const [defaultValue, setDefaultValue] = React.useState<any>(value);
 
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+    setDefaultValue(value);
+    onChange(value);
+  };
 
-    const [defaultValue, setDefaultValue] = React.useState<any>(value);
-
-
-    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const {value} = event.target;
-        setDefaultValue(value);
-        onChange(value);
-    };
-
-    return (
-
-        <div style={{display: 'flex'}}>
-            {prefix && <div>{prefix}</div>}
-            <input
-                type="range"
-                min={min} max={max}
-                value={defaultValue}
-                onChange={handleChange}
-                disabled={disabled}
-                className={inputClass}
-                aria-label={ariaLabel || 'input-slider'}
-                {...props}
-            />
-            {suffix && <div>{suffix}</div>}
-        </div>
-
-    );
+  return (
+    <div style={{ display: 'flex' }}>
+      {prefix && <div>{prefix}</div>}
+      <input
+        type="range"
+        min={min}
+        max={max}
+        value={defaultValue}
+        onChange={handleChange}
+        disabled={disabled}
+        className={inputClass}
+        aria-label={ariaLabel || 'input-slider'}
+        {...props}
+      />
+      {suffix && <div>{suffix}</div>}
+    </div>
+  );
 };
-
--- src/range/__tests__/Range.test.tsx
+++ src/range/__tests__/Range.test.tsx
@@ -4,55 +4,45 @@ import '@testing-library/jest-dom';
 import { Range } from '../Range';
 
 beforeEach(() => {
-    jest.useFakeTimers();
+  jest.useFakeTimers();
 });
 
 afterEach(() => {
-    jest.clearAllTimers();
+  jest.clearAllTimers();
 });
 
 describe('Range', () => {
-    it('should render', () => {
-        render(<Range/>);
-        expect(screen.getByLabelText('input-slider')).toBeInTheDocument();
-    });
+  it('should render', () => {
+    render(<Range />);
+    expect(screen.getByLabelText('input-slider')).toBeInTheDocument();
+  });
 
-    it('should render disable range', () => {
-        render(<Range disabled={true}/>);
-        const range: any = screen.getByLabelText('input-slider');
-        expect(range.disabled).toBeTruthy();
-    });
+  it('should render disable range', () => {
+    render(<Range disabled={true} />);
+    const range: any = screen.getByLabelText('input-slider');
+    expect(range.disabled).toBeTruthy();
+  });
 
-    it('should render prefix image', () => {render(
-        <Range
-            prefix={
-                <img aria-label="prefixImg" alt="" src="" />
-            }/>
-    );
-        expect(screen.getByLabelText('prefixImg')).toBeInTheDocument();
-    });
+  it('should render prefix image', () => {
+    render(<Range prefix={<img aria-label="prefixImg" alt="" src="" />} />);
+    expect(screen.getByLabelText('prefixImg')).toBeInTheDocument();
+  });
 
-    it('should render suffix image', () => {
-        render(
-            <Range
-                suffix={
-                    <img aria-label="suffixImg" alt="" src="" />
-                } />
-        );
-        expect(screen.getByLabelText('suffixImg')).toBeInTheDocument();
-    });
+  it('should render suffix image', () => {
+    render(<Range suffix={<img aria-label="suffixImg" alt="" src="" />} />);
+    expect(screen.getByLabelText('suffixImg')).toBeInTheDocument();
+  });
 
-    it('should accept className as attribute', () => {
-        render(<Range inputClass="test" />);
-        const range: any = screen.getByRole('slider');
-        expect(range.getAttribute('class')).toBe('test');
-    });
+  it('should accept className as attribute', () => {
+    render(<Range inputClass="test" />);
+    const range: any = screen.getByRole('slider');
+    expect(range.getAttribute('class')).toBe('test');
+  });
 
-    it('should call onChange function', () => {
-        const handleChange = jest.fn();
-        render(<Range value={50} onChange={handleChange} />)
-        fireEvent.change(screen.getByRole('slider'), { target: { value: '80' }})
-        expect(handleChange).toHaveBeenCalled();
-    });
-
+  it('should call onChange function', () => {
+    const handleChange = jest.fn();
+    render(<Range value={50} onChange={handleChange} />);
+    fireEvent.change(screen.getByRole('slider'), { target: { value: '80' } });
+    expect(handleChange).toHaveBeenCalled();
+  });
 });
