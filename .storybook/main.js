a9a726d44d76de40cf3e1633764111bdc48a7686
Szilvi Horvath <szilvi.horvath@capgemini.com>
2023-01-17 16:45:56 +0000

    fix: installed webpack 5 for storybook to enable build with node v18

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,14 @@
 module.exports = {
+  core: {
+    builder: 'webpack5',
+  },
   stories: ['../stories/**/*.stories.@(mdx)'],
   addons: [
-      '@storybook/addon-links', 
-      '@storybook/addon-essentials', 
-      '@storybook/addon-docs', 
-      '@storybook/addon-controls', 
-      'storybook-css-modules-preset',
-      '@storybook/addon-a11y'
-    ],
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-docs',
+    '@storybook/addon-controls',
+    'storybook-css-modules-preset',
+    '@storybook/addon-a11y',
+  ],
 };
