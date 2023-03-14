22d52f230fa365087f25d1a969bf465e03f231d8
Zurico.Daniele_DEFRA <daniele.zurico@gmail.com>
2023-03-14 14:59:07 +0000

    feat: update storybook menu

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,28 +1,9 @@
 // https://storybook.js.org/docs/react/writing-stories/parameters#global-parameters
 import '../stories/govUkStyle.css';
 export const parameters = {
-  // https://storybook.js.org/docs/react/essentials/actions#automatically-matching-args
-  actions: { argTypesRegex: '^on.*' },
   options: {
-    storySort: (a, b) => {
-      let order = 2000;
-      if (a[0].includes('dcxlibrary-form')) {
-        return order;
-      } else if (a[0].includes('progress')) {
-        return order--;
-      } else if (a[0].includes('copy')) {
-        return order--;
-      } else if (a[0].includes('tabs')) {
-        return order--;
-      } else if (a[0].includes('table')) {
-        return order--;
-      } else if (a[0].includes('details')) {
-        return order--;
-      } else if (a[0].includes('utils')) {
-        return order--;
-      } else {
-        return 0;
-      }
+    storySort: {
+      order: ['DCXLibrary', ['Introduction','Utils', 'Form', 'CopyToClipboard', 'Details', 'Tabs', 'Table', 'Changelog']]
     },
   },
 };
