1b674cd7041b356723ea4f55b3e42ab6cf422dbe
Daniele Zurico <daniele.zurico@gmail.com>
2023-04-25 16:05:10 +0100

    [Design-System-FormSelect] Build the design system for the FormSelect component (#421)
    * feat: add design system
    
    * feat: fix conflicts
    
    * feat: final push
    
    * feat: final push
    
    * feat: revert some bad merge
    
    * feat: revert some bad merge
    
    * feat: add chromatic build

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -3,7 +3,38 @@ import '../stories/govUkStyle.css';
 export const parameters = {
   options: {
     storySort: {
-      order: ['DCXLibrary', ['Introduction','Utils', 'Form', 'CopyToClipboard', 'Details', 'Tabs', 'Table', 'Changelog']]
+      order: ['DCXLibrary',
+        [
+          'Introduction',
+          'Utils',
+          'Form', ['Select', ['documentation', 'live', 'Default', 'Design-System', 'Class-Based']],
+          'CopyToClipboard',
+          'Details',
+          'Tabs',
+          'Table',
+          'Changelog'
+        ]
+      ]
     },
-  },
+  }
 };
+
+//It will allow to refresh the iframe all the time you move from one story to another - https://github.com/storybookjs/storybook/issues/16016
+let storyId;
+let storyTitle;
+export const decorators = [
+  (storyFn, context) => {
+    console.log('context.title:', context.title);
+    console.log('storyTitle:', storyTitle);
+    if (storyTitle && context.title !== storyTitle) {
+      document.location.reload();
+      console.log('first')
+    } else if (storyId && context.id !== storyId && context.title !== storyTitle) {
+      document.location.reload();
+      console.log('second')
+    }
+    storyId = context.id;
+    storyTitle = context.title;
+    return storyFn();
+  }
+];
