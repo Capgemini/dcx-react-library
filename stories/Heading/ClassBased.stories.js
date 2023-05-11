90014e84baf8fdf3557c72e40ec30a9994d7eb55
aismaelcap <aismael@aismaels-MacBook-Pro.local>
2023-05-11 13:42:20 +0100

    refactor ClassBased and create Headings for all levels

--- stories/Heading/ClassBased.stories.js
+++ stories/Heading/ClassBased.stories.js
@@ -1,6 +1,6 @@
 import { Heading } from '../../src/heading/Heading';
 /**
- * In this section we're using the heading component providing the **GovUk style** passing the relative `className.
+ * In this section we're using the heading component providing the **GovUk style** passing the relative className.
  * Feel free to use your own css to style the paragraph as you prefer.
  */
 export default {
@@ -14,11 +14,20 @@ export default {
     tags: ['autodocs'],
 };
 
-export const Basic = {
-    name: 'Basic',
-    args: {
-        className: 'govuk-body',
-        label: 'This is the content of the Heading.',
-        level: '1',
-    },
-};
+function createHeading(level) {
+    return {
+        name: `Basic H${level}`,
+        args: {
+            className: 'govuk-body',
+            label: `This is a Level ${level} Header`,
+            level,
+        },
+    }
+}
+
+export const BasicH1 = createHeading('1')
+export const BasicH2 = createHeading('2')
+export const BasicH3 = createHeading('3')
+export const BasicH4 = createHeading('4')
+export const BasicH5 = createHeading('5')
+export const BasicH6 = createHeading('6')
