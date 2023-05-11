85c19712edf70dfc02d6998da1b04df06a7269ae
aismaelcap <aismael@aismaels-MacBook-Pro.local>
2023-05-11 15:37:17 +0100

    add specific testing

--- src/heading/__test__/Heading.test.tsx
+++ src/heading/__test__/Heading.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Heading, HeadingLevel } from '../Heading';
+import { Heading } from '../Heading';
 import {
     render,
     screen,
@@ -8,21 +8,58 @@ import '@testing-library/jest-dom';
 
 
 describe('Heading', () => {
-    it('should render', () => {
-        const headingLevels: HeadingLevel[] = '123456'.split('') as HeadingLevel[];
-        headingLevels.forEach(level => {
-            const { getByText, container } = render(<Heading id="first" className="heading" label={`This is a Level ${level} Heading`} level={`${level}`} />);
-            const heading = getByText(`This is a Level ${level} Heading`);
-            expect(heading).toBeTruthy();
 
-            expect(container.querySelector(`h${level}`)).toBeInTheDocument();
-
-
-            expect(container.querySelector(`.dcx-heading-${level}`)).toBeInTheDocument();
-        });
+    it('h1 rendered', () => {
+        const { container } = render(<Heading id="test" className="" label="This is a Level 1 Heading" level="1" />);
+        expect(container.querySelector('h1')).toBeInTheDocument();
     });
 
-    it('Renders the given content in the header.', () => {
+    it('h2 rendered', () => {
+        const { container } = render(<Heading id="test" className="" label="This is a Level 2 Heading" level="2" />);
+        expect(container.querySelector('h2')).toBeInTheDocument();
+    });
+
+    it('h3 rendered', () => {
+        const { container } = render(<Heading id="test" className="" label="This is a Level 3 Heading" level="3" />);
+        expect(container.querySelector('h3')).toBeInTheDocument();
+    });
+
+    it('h4 rendered', () => {
+        const { container } = render(<Heading id="test" className="" label="This is a Level 4 Heading" level="4" />);
+        expect(container.querySelector('h4')).toBeInTheDocument();
+    });
+
+    it('h5 rendered', () => {
+        const { container } = render(<Heading id="test" className="" label="This is a Level 5 Heading" level="5" />);
+        expect(container.querySelector('h5')).toBeInTheDocument();
+    });
+
+    it('h6 rendered', () => {
+        const { container } = render(<Heading id="test" className="" label="This is a Level 6 Heading" level="6" />);
+        expect(container.querySelector('h6')).toBeInTheDocument();
+    });
+
+    it('id attribute rendered', () => {
+        const { container } = render(<Heading id="first" className="" label="This is a Level 1 Heading" level="1" />);
+        expect(container.querySelector('#first')).toBeInTheDocument();
+    });
+
+    it('dcx-heading classes are rendered', () => {
+        const { container } = render(<Heading id="first" className="" label="This is a Level 1 Heading" level="1" />);
+        expect(container.querySelector('.dcx-heading.dcx-heading-1')).toBeInTheDocument();
+    });
+
+    it('dcx-heading class and additional classes are rendered', () => {
+        const { container } = render(<Heading id="first" className="test-heading" label="This is a Level 2 Heading" level="2" />);
+        expect(container.querySelector('.dcx-heading.dcx-heading-2.test-heading')).toBeInTheDocument();
+    });
+
+    it('the ability to provide other props', () => {
+        const { container } = render(<Heading id="first" className="heading" label="This is a Level 1 Heading" level="1" props={{ 'custom-attribute': "testHeader" }} />);
+        expect(container.querySelector('h1[custom-attribute="testHeader"]')).toBeInTheDocument();
+    });
+
+    it('Renders content in Header', () => {
         render(<Heading id="first" className="heading" label="This is a Level 1 Heading" level="1" />);
         expect(screen.getByText('This is a Level 1 Heading')).toBeInTheDocument();
     });
