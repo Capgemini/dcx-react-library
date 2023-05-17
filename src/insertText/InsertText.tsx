ab3a89ecb2c5c829b7914b8c7b67e3379e64387a
kavithakurella-dev <132873865+kavithakurella-dev@users.noreply.github.com>
2023-05-17 15:51:35 +0530

    resolved pr comments

--- src/insertText/InsertText.tsx
+++ src/insertText/InsertText.tsx
@@ -9,12 +9,19 @@ export type TextProps = {
   /**
    * select value which is added by user
    */
-  value?: string;
+  value?: 'insert text';
+  /**
+   * It will pass an id to the InsertText element
+   */
 
   id?: string;
+  /**
+   * Additional props/attributes
+   */
+  props?: React.HTMLAttributes<HTMLHeadingElement>;
 };
 
-export const InsertText = ({ value, className, id, ...props }: TextProps) => {
+export const InsertText = ({ value, className, id, props }: TextProps) => {
   const classes = classNames(['dcx-insert-text', className]);
 
   // Return a div element with the dynamic class name and any additional props passed to the component
--- src/insertText/__test__/InsertText.test.tsx
+++ src/insertText/__test__/InsertText.test.tsx
@@ -7,6 +7,37 @@ describe('InsertText', () => {
   it('should render', () => {
     render(<InsertText value="insert text" id="user-defined-id" />);
 
-    expect(screen.getByText(' text')).toBeInTheDocument();
+    expect(screen.getByText('insert text')).toBeInTheDocument();
+  });
+
+  it('should have the class dcx-insert-text if the user does not specify any additional class', () => {
+    render(<InsertText value="insert text" id="user-defined-id" />);
+    expect(screen.getByText('insert text')).toHaveClass('dcx-insert-text');
+  });
+
+  it('should have the class dcx-insert-text and the user based class if the user specifies any additional class', () => {
+    render(
+      <InsertText
+        value="insert text"
+        id="user-defined-id"
+        className="my-custom-class"
+      />
+    );
+    expect(screen.getByText('insert text')).toHaveClass('dcx-insert-text');
+    expect(screen.getByText('insert text')).toHaveClass('my-custom-class');
+  });
+
+  it('should be able to pass some extra properties', () => {
+    const { container } = render(
+      <InsertText
+        value="insert text"
+        id="user-defined-id"
+        props={{ style: { color: 'red' } }}
+      />
+    );
+    const inserttextElement =
+      container.getElementsByClassName('dcx-insert-text');
+    const style = window.getComputedStyle(inserttextElement[0]);
+    expect(style.color).toBe('red');
   });
 });
