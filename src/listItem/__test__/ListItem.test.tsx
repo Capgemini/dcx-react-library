c1cb695fca8ca96df6674cbc233b1a1653f0bd77
JadhavJeet <jeet.d.jadhav@capgemini.com>
2023-05-17 14:07:24 +0530

    folder structure and test case scenarios added

---
+++ src/listItem/__test__/ListItem.test.tsx
@@ -0,0 +1,18 @@
+import '@testing-library/jest-dom';
+
+describe('ListItem', () => {
+  // for List component
+  it('should render', () => {});
+
+  it('should render with the dcx-list className when no className Provided', () => {});
+  it('should be able to render the dcx-list and the user specific className', () => {});
+  it('should be able to pass itemClassName prop', () => {});
+  it('should be able to pass type prop with either ordered or unordered value', () => {});
+  it('should be able to pass some extra properties', () => {});
+
+  //   for nested ListItem component
+  it('should render with the dcx-list-item className when no className Provided', () => {});
+  it('should be able to render the dcx-list-item and the user specific className', () => {});
+  it('should be able to pass value prop', () => {});
+  it('should be able to pass some extra properties', () => {});
+});
