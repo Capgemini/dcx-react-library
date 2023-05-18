21317baedacebb2bbba7ce1e066f0a74aaa718bf
JadhavJeet <jeet.d.jadhav@capgemini.com>
2023-05-18 16:52:49 +0530

    added comments and updated code

--- src/list/List.tsx
+++ src/list/List.tsx
@@ -1,31 +1,53 @@
 import React from 'react';
 import { classNames } from '../common';
 
-type list = {
+export type list = {
+  /**
+   * Type property to specify ordered or unordered lists
+   */
   type: 'unordered' | 'ordered';
+  /**
+   * A CSS class for styling list
+   */
   className?: string;
+  /**
+   * A CSS class for applying same styling to all the listItems
+   */
   itemClassName?: string;
+  /**
+   * Additional props/attributes
+   */
   listProps?:
     | React.HTMLAttributes<HTMLOListElement>
     | React.HTMLAttributes<HTMLUListElement>;
 };
 
-type listItem = {
+export type listItem = {
+  /**
+   * define the value of the link
+   */
   value?: string;
-  listItemProps?: React.HTMLProps<HTMLLIElement>;
+  /**
+   * A CSS class for styling list
+   */
   className?: string;
+  /**
+   * Additional props/attributes to pass in listitems
+   */
+  listItemProps?: React.HTMLProps<HTMLLIElement>;
 };
 
-export const List = ({ type, className, listProps, itemClassName }: list) =>
-  type === 'unordered' ? (
-    <ul className={classNames(['dcx-list', className])} {...listProps}>
+type ElementType = 'ul' | 'ol';
+
+export const List = ({ type, className, listProps, itemClassName }: list) => {
+  const Element: ElementType = type === 'unordered' ? 'ul' : 'ol';
+
+  return (
+    <Element className={classNames(['dcx-list', className])} {...listProps}>
       <ListItem value="list text" className={itemClassName} />
-    </ul>
-  ) : (
-    <ol className={classNames(['dcx-list', className])} {...listProps}>
-      <ListItem value="list text" className={itemClassName} />
-    </ol>
+    </Element>
   );
+};
 
 export const ListItem = ({ value, listItemProps, className }: listItem) => (
   <li className={classNames(['dcx-list-item', className])} {...listItemProps}>
