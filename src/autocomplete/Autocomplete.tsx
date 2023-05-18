e54d2c465f0dc362ed3ff710517352648310f8da
Ahmet-K <ahmet.kio@hotmail.com>
2023-05-18 11:36:57 +0100

    fix: {{ Autocomplete component - changing defaultValue prop has no affect }}

--- src/autocomplete/Autocomplete.tsx
+++ src/autocomplete/Autocomplete.tsx
@@ -366,6 +366,10 @@ export const Autocomplete = ({
     }
   }, [options, onChange]);
 
+  React.useEffect(() => {
+    setUserInput(defaultValue);
+  }, [defaultValue]);
+  
   const handleClick = (evt: React.FormEvent<HTMLInputElement>) => {
     setActiveOption(0);
     setFilterList([]);
