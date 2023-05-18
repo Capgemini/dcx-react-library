2c6878162da9b3bbcb3096a148dc8f03419f6f04
Zurico.Daniele_DEFRA <daniele.zurico@gmail.com>
2023-05-18 10:18:55 +0100

    feat: improve documentation

--- src/insertText/InsertText.tsx
+++ src/insertText/InsertText.tsx
@@ -1,30 +1,36 @@
 import React from 'react';
 import { classNames } from '../common';
 
-export type TextProps = {
+export type InsertTextProps = {
   /**
    * specify a custom class name to be applied to the Insert-text
    */
   className?: string;
+
   /**
    *value specified by user
    */
   value: 'insert text';
+
   /**
    * It will pass an id to the InsertText element
    */
-
   id?: string;
+
   /**
    * Additional props/attributes
    */
   props?: React.HTMLAttributes<HTMLDivElement>;
 };
 
-export const InsertText = ({ value, className, id, props }: TextProps) => {
+export const InsertText = ({
+  value,
+  className,
+  id,
+  props,
+}: InsertTextProps) => {
   const classes = classNames(['dcx-insert-text', className]);
 
-  // Return a div element with the dynamic class name and any additional props passed to the component
   return (
     <div className={classes} id={id} {...props}>
       {value}
