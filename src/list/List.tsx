5d38652d6b11e1ee1d6c2ae25a075375f8ad73d8
JadhavJeet <jeet.d.jadhav@capgemini.com>
2023-05-18 14:50:59 +0530

    Added required test cases

--- src/list/List.tsx
+++ src/list/List.tsx
@@ -1,8 +1,34 @@
 import React from 'react';
+import { classNames } from '../common';
+
+type list = {
+  type: 'unordered' | 'ordered';
+  className?: string;
+  itemClassName?: string;
+  listProps?:
+    | React.HTMLAttributes<HTMLOListElement>
+    | React.HTMLAttributes<HTMLUListElement>;
+};
 
 type listItem = {
   value?: string;
+  listItemProps?: React.HTMLProps<HTMLLIElement>;
+  className?: string;
 };
 
-export const List = () => <></>;
-export const ListItem = ({ value }: listItem) => <>{value}</>;
+export const List = ({ type, className, listProps, itemClassName }: list) =>
+  type === 'unordered' ? (
+    <ul className={classNames(['dcx-list', className])} {...listProps}>
+      <ListItem value="list text" className={itemClassName} />
+    </ul>
+  ) : (
+    <ol className={classNames(['dcx-list', className])} {...listProps}>
+      <ListItem value="list text" className={itemClassName} />
+    </ol>
+  );
+
+export const ListItem = ({ value, listItemProps, className }: listItem) => (
+  <li className={classNames(['dcx-list-item', className])} {...listItemProps}>
+    {value}
+  </li>
+);
--- src/list/__test__/List.test.tsx
+++ src/list/__test__/List.test.tsx
@@ -1,28 +1,74 @@
 import '@testing-library/jest-dom';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import React from 'react';
-import { ListItem } from '../List';
+import { List, ListItem } from '../List';
 
 describe('ListItem', () => {
-  it('should render', () => {
+  it('should display the text of the listItem', () => {
     render(<ListItem value="listItem_text" />);
     expect(screen.getByText('listItem_text')).toBeInTheDocument();
   });
 
-  //   for nested ListItem component
-  xit('should display the text of the listItem', () => {});
-  xit('should render ul if the list type is unordered list', () => {});
+  it('should render with the dcx-list-item className when no className is Provided', () => {
+    const { container } = render(<ListItem />);
+    expect(container.querySelector('.dcx-list-item')).toBeInTheDocument();
+  });
 
-  //
-  xit('should render with the dcx-list-item className when no className is Provided', () => {});
-  xit('should be able to render the dcx-list-item and the user specific className', () => {});
-  xit('should be able to pass value prop', () => {});
-  xit('should be able to pass some extra properties', () => {});
+  it('should be able to render the dcx-list-item and the user specific className', () => {
+    const { container } = render(<ListItem className="myStyle" />);
+    expect(container.querySelector('.dcx-list-item')).toBeInTheDocument();
+    expect(container.querySelector('.myStyle')).toBeInTheDocument();
+  });
 
-  // for List component7
-  xit('should render with the dcx-list className when no className is Provided', () => {});
-  xit('should be able to render the dcx-list and the user specific className', () => {});
-  xit('should be able to pass itemClassName prop', () => {});
-  xit('should be able to pass type prop with either ordered or unordered value', () => {});
-  xit('should be able to pass some extra properties', () => {});
+  it('should be able to pass some extra properties', () => {
+    const { container } = render(
+      <ListItem value="text" listItemProps={{ style: { color: 'red' } }} />
+    );
+    const labelElement = container.getElementsByClassName('dcx-list-item');
+    const style = window.getComputedStyle(labelElement[0]);
+    expect(style.color).toBe('red');
+  });
+
+  it('should trigger a callback function when clicked', () => {
+    const onClickMock = jest.fn();
+    render(
+      <ListItem
+        value="listItem_text"
+        listItemProps={{ onClick: onClickMock() }}
+      />
+    );
+    const listItemElement = screen.getByText('listItem_text');
+    fireEvent.click(listItemElement);
+    expect(onClickMock).toHaveBeenCalledTimes(1);
+  });
+
+  it('should render with the dcx-list className when no className is Provided', () => {
+    const { container } = render(<List type="unordered" />);
+    expect(container.querySelector('.dcx-list')).toBeInTheDocument();
+  });
+
+  it('should be able to render the dcx-list and the user specific className', () => {
+    const { container } = render(<List type="unordered" className="myStyle" />);
+    expect(container.querySelector('.dcx-list')).toBeInTheDocument();
+    expect(container.querySelector('.myStyle')).toBeInTheDocument();
+  });
+
+  it('should render ul if the list type is unordered list', () => {
+    render(<List type="unordered" />);
+    const unorderedList = screen.getByRole('list');
+    expect(unorderedList.tagName).toBe('UL');
+  });
+
+  it('should render ol if the list type is ordered list', () => {
+    render(<List type="ordered" />);
+    const orderedList = screen.getByRole('list');
+    expect(orderedList.tagName).toBe('OL');
+  });
+
+  it('should be able to pass itemClassName prop', () => {
+    const { container } = render(
+      <List type="unordered" itemClassName="myStyle" />
+    );
+    expect(container.querySelector('.myStyle')).toBeInTheDocument();
+  });
 });
