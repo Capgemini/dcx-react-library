6fd0c8e5079967ef526c494a8ec4b544a8292ceb
Yunus FIRAT <yunusfirat44@gmail.com>
2023-05-25 16:48:14 +0100

    files has been updated.

--- src/paragraph/__tests__/Paragraph.test.tsx
+++ src/paragraph/__tests__/Paragraph.test.tsx
@@ -4,18 +4,22 @@ import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 
 describe('Paragraph', () => {
-  it('should render the given content in the paragraph.', () => {
-    const { container } = render(<Paragraph value="paragraph text" />);
+  
+  it('should render', () => {
+    const { container } = render(<Paragraph value="paragraph text"/>);
     expect(container.querySelector('p')).toBeInTheDocument();
+  });
+  it('should allow to pass a value', () => {
+    render(<Paragraph className="paragraph" value="paragraph text" />);
     expect(screen.getByText('paragraph text')).toBeInTheDocument();
   });
-  
+
   it('should provide the ability to specify arbitrary props', () => {
     const { container } = render(
       <Paragraph
         className="paragraph"
         value="paragraph text"
-        props={{ id: 'my-paragraph' }}
+        props= {{ id:'my-paragraph'}}
       />
     );
     expect(container.querySelector('#my-paragraph')).toBeInTheDocument();
