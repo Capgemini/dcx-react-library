420c364f33b687fb9e412efff6702c2c28fb2cea
kavithakurella-dev <132873865+kavithakurella-dev@users.noreply.github.com>
2023-05-25 14:58:56 +0530

    KeyboardInput

--- src/keyBoard/KeyboardInput.tsx
+++ src/keyBoard/KeyboardInput.tsx
@@ -11,6 +11,10 @@ export type KeyboardInputProps = {
    */
   children: React.ReactNode;
   /**
+   * It will pass an id to the KeyboardInput element
+   */
+  id?: string;
+  /**
    * Additional props/attributes
    */
   props?: React.HTMLAttributes<HTMLElement>;
@@ -19,13 +23,14 @@ export type KeyboardInputProps = {
 export const KeyboardInput = ({
   children,
   className,
+  id,
   props,
 }: KeyboardInputProps) => {
-  const classes = classNames(['dcx-keyboard-input', className]);
+  const classes = classNames(['dcx-keyboard-Input', className]);
 
   // Return a kbd element with the dynamic class name and any additional props passed to the component
   return (
-    <kbd className={classes} {...props}>
+    <kbd className={classes} id={id} {...props}>
       {children}
     </kbd>
   );
