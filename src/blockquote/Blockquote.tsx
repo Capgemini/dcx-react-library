23844dd50ca111553a14e9f0de0128c3c5d76e75
Yunus FIRAT <yunusfirat44@gmail.com>
2023-05-31 00:54:17 +0100

    blockquote file.

---
+++ src/blockquote/Blockquote.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { classNames } from '../common/utils';
+
+
+type BlockquoteProps = {
+    /**
+   * optional CSS class name
+   */
+    className?: string;
+    /**
+   * content of the blockquotes
+   */
+    text: string | number;
+    /**
+   * footer of the blockquotes
+   */
+    footer: string | number;
+    /**
+   * Additional props/attributes
+   */
+    props?: React.HTMLAttributes<HTMLParagraphElement>;
+};
+
+
+export const Blockquote = ({ className, text, footer, props }: BlockquoteProps) => {
+    const dynamicClassName = classNames(['dcx-blockquote', className]);
+
+    return (
+        <blockquote>
+            <p className={dynamicClassName} {...props}>
+                {text}
+            </p>
+            <footer>{footer}</footer>
+        </blockquote>
+        );
+};
