3a0d09ab820e9d783e8b06cd44e017d58fb5cdb6
Yunus FIRAT <yunusfirat44@gmail.com>
2023-05-31 17:28:18 +0100

    styled with classname

--- stories/BlockQuote/ClassBased.stories.js
+++ stories/BlockQuote/ClassBased.stories.js
@@ -1,4 +1,5 @@
 import { Blockquote } from '../../src/blockquote/Blockquote';
+import './style.css';
 /**
  * In this section we're using the Blockquote component providing the relative `className, text and footer` . Feel free to use your own css and style the Blockquote as you prefer.
  */
@@ -13,24 +14,12 @@ export default {
   tags: ['autodocs'],
 };
 
-const style = {
-    margin: '0',
-    padding: '15px',
-    background: '#eee',
-    borderRadius: '5px',
-    width: '500px'
+export const Basic = {
+  name: 'Basic',
+  args: {
+    className: 'blockquote',
+    text: 'This is the content of the blockquote.',
+    footer: 'This is the footer of the blockquote.'
+  },
 };
 
-export const Styled = {
-  name: 'with some style',
-  render: function() {
-    return (
-      <div style={style}>
-        <figure>
-          <Blockquote text='We are such stuff as dreams are made on.' cite="https://www.twinkl.co.uk/homework-help/famous-figures-homework-help/shakespeare-facts-for-kids/top-10-famous-quotes-from-shakespeares-plays"/>
-          <figcaption>—Act IV, <cite>Scene I</cite></figcaption>
-        </figure>
-      </div>
-    );
-}
-};
