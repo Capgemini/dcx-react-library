b07dbcd9b2e79d5095e57e7a08da86de56faacac
Yunus FIRAT <yunusfirat44@gmail.com>
2023-05-31 14:44:49 +0100

    some comments has been fixed.

--- stories/BlockQuote/ClassBased.stories.js
+++ stories/BlockQuote/ClassBased.stories.js
@@ -1,7 +1,6 @@
 import { Blockquote } from '../../src/blockquote/Blockquote';
 /**
- * In this section we're using the blockquote component providing the **GovUk style** passing the relative `className.
- * Feel free to use your own css to style the blockquote as you prefer.
+ * In this section we're using the Blockquote component providing the relative `className, text and footer` . Feel free to use your own css and style the Blockquote as you prefer.
  */
 export default {
   title: 'DCXLibrary/Typography/Blockquote/Class based',
@@ -17,7 +16,7 @@ export default {
 export const Basic = {
   name: 'Basic',
   args: {
-    className: 'blockquote-reverse',
+    className: 'blockquote',
     text: 'This is the content of the blockquote.',
     footer: 'This is the footer of the blockquote.'
   },
