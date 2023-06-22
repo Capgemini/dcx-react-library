a6c32f99609ac5a9e2894968ca5dab7878c50beb
Yunus FIRAT <yunusfirat44@gmail.com>
2023-06-22 14:42:31 +0100

    update.

--- stories/FormSelect/design-system/Default.stories.js
+++ stories/FormSelect/design-system/Default.stories.js
@@ -1,10 +1,11 @@
-import { Paragraph } from '../../../src/paragraph/Paragraph';
+import { FormSelect } from '../../../src/formSelect/FormSelect';
+
 /**
-* Here we display the component in its natural paragraph, importing only the base Design System styles.
+* Here we display the component in its natural form, importing only the base Design System styles.
 */
 export default {
-  title:'DCXLibrary/Typography/Paragraph/Design system/Default',
-  component: Paragraph,
+  title:"DCXLibrary/Form/Select/Design system/Default",
+  component: FormSelect,
   decorators:[
     (getStory) => {
       require('../../../dist/design-system/index.css');
@@ -21,8 +22,54 @@ export default {
 export const Default = {  
   name: 'Default',
   args: {
-    className: 'govuk-body',
-    value: 'This is the content of the paragraph.',
-  },
+    label:"My label",
+    options:['Option 1', 'Option 2'],
+    nullOption:"Select an option...",
+  }
 };
 
+export const Hint = {  
+  name: 'Hint',
+  args: {
+    label:"My label",
+    options:['Option 1', 'Option 2'],
+    nullOption:"Select an option...",
+    hint:{
+      text: 'This is and example of hintText/description of what we need from you',
+    },
+  }
+};
+
+export const Error = {  
+  name: 'Error',
+  args: {
+    label:"My label",
+    options:['Option 1', 'Option 2'],
+    nullOption:"Select an option...",
+    errorMessage:"Some error",
+  }
+};
+
+export const HintAndError = {  
+  name: 'Hint and error',
+  args: {
+    label:"My label",
+    options:['Option 1', 'Option 2'],
+    nullOption:"Select an option...",
+    hint:{
+      text: 'This is and example of hintText/description of what we need from you',
+    },
+    errorMessage:"Some error",
+
+  }
+};
+
+export const Value = {  
+  name: 'With Value',
+  args: {
+    label:"My label",
+    options:['Option 1', 'Option 2'],
+    nullOption:"Select an option...",
+    value:"Option 1",
+  }
+};
--- stories/paragraph/design-system/Default.stories.js
+++ stories/paragraph/design-system/Default.stories.js
@@ -1,11 +1,10 @@
-import { FormSelect } from '../../../src/formSelect/FormSelect';
-
+import { Paragraph } from '../../../src/paragraph/Paragraph';
 /**
-* Here we display the component in its natural form, importing only the base Design System styles.
+* Here we display the component in its natural paragraph, importing only the base Design System styles.
 */
 export default {
-  title:"DCXLibrary/Form/Select/Design system/Default",
-  component: FormSelect,
+  title:'DCXLibrary/Typography/Paragraph/Design system/Default',
+  component: Paragraph,
   decorators:[
     (getStory) => {
       require('../../../dist/design-system/index.css');
@@ -22,54 +21,8 @@ export default {
 export const Default = {  
   name: 'Default',
   args: {
-    label:"My label",
-    options:['Option 1', 'Option 2'],
-    nullOption:"Select an option...",
-  }
+    className: 'govuk-body',
+    value: 'This is the content of the paragraph.',
+  },
 };
 
-export const Hint = {  
-  name: 'Hint',
-  args: {
-    label:"My label",
-    options:['Option 1', 'Option 2'],
-    nullOption:"Select an option...",
-    hint:{
-      text: 'This is and example of hintText/description of what we need from you',
-    },
-  }
-};
-
-export const Error = {  
-  name: 'Error',
-  args: {
-    label:"My label",
-    options:['Option 1', 'Option 2'],
-    nullOption:"Select an option...",
-    errorMessage:"Some error",
-  }
-};
-
-export const HintAndError = {  
-  name: 'Hint and error',
-  args: {
-    label:"My label",
-    options:['Option 1', 'Option 2'],
-    nullOption:"Select an option...",
-    hint:{
-      text: 'This is and example of hintText/description of what we need from you',
-    },
-    errorMessage:"Some error",
-
-  }
-};
-
-export const Value = {  
-  name: 'With Value',
-  args: {
-    label:"My label",
-    options:['Option 1', 'Option 2'],
-    nullOption:"Select an option...",
-    value:"Option 1",
-  }
-};
