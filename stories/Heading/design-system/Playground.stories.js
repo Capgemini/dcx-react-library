a6e1a95b8550ad053624b62f2741e0a4358a318b
SOUNDAR-A <138137507+SOUNDAR-A@users.noreply.github.com>
2023-09-01 18:14:31 +0530

    feat: add heading design system styles (#521)

---
+++ stories/Heading/design-system/Playground.stories.js
@@ -0,0 +1,26 @@
+/* eslint-disable import/no-anonymous-default-export */
+import { Heading } from '../../../src/heading/Heading';
+
+export default {
+  title: 'DCXLibrary/Typography/Heading/Design system',
+  component: Heading,
+  decorators: [
+    (getStory) => {
+      require('../../../dist/design-system/index.css');
+      return getStory();
+    }
+  ],
+  parameters: {
+    options: { showPanel: true },
+    actions: { disable: true },
+  }
+};
+
+
+export const Playground = {
+  name: 'Playground',
+  args: {
+    label: 'This is the content of the heading',
+    level: 'h1',
+  }
+};
