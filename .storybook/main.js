9082b8a2d05dec9b6fb2172d8232348c71c58d8c
Daniele Zurico <daniele.zurico@gmail.com>
2023-09-15 16:24:33 +0100

    Feature/release notes (#527)
    * feat: update release notes
    
    * feat: update release notes

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,20 +2,23 @@ process.env.DESIGN_TOKEN_GLOB = "**/tokens.css";
 const config = {
   stories: ['../stories/**/*.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
-    '@storybook/addon-links', 
-    '@storybook/addon-essentials', 
-    '@storybook/addon-docs', 
-    '@storybook/addon-controls', 
-    'storybook-css-modules-preset', 
-    '@storybook/addon-a11y', {
-      name: "storybook-design-token",
-      options: {
-        preserveCSSVars: false
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-docs',
+    '@storybook/addon-controls',
+    'storybook-css-modules-preset',
+    '@storybook/addon-a11y',
+    {
+        name: "storybook-design-token",
+        options: {
+          preserveCSSVars: false
+        },
+      docs: {
+        autodocs: "tag",
       },
-    docs: {
-      autodocs: "tag",
     },
-  }],
+    '@storybook/addon-mdx-gfm'
+  ],
   framework: {
     name: '@storybook/react-webpack5',
     options: {}
