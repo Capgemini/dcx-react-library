651db7df82614597a5617c694986bdd663ec25cb
ybayrakt <ybayrakt@adcs-MBP.cust.communityfibre.co.uk>
2023-10-06 12:02:28 +0100

    style: code snippet design system added

---
+++ stories/CodeSnippet/design-system/DarkTheme.stories.js
@@ -0,0 +1,60 @@
+import { CodeSnippet } from '../../../src/codesnippet/CodeSnippet';
+// eslint-disable-next-line import/no-webpack-loader-syntax
+import style from '!raw-loader!../../themes/dark.theme.css';
+import { LiveProvider, LiveEditor } from 'react-live';
+import { StorybookUtils } from '../../../core/storybook/StorybookUtils';
+
+/**
+* This a theme showcases how to customize the component so it can be used on dark backgrounds.
+*/
+export default {
+  title: 'DCXLibrary/Typography/CodeSnippet/Design system/Dark',
+  component: CodeSnippet,
+  decorators: [
+    (getStory) => {
+      require('../../../dist/design-system/index.css');
+      require('../../themes/dark.theme.css');
+      return getStory();
+    }
+  ],
+  parameters: {
+    options: { showPanel: true },
+    actions: { disable: true },
+  },
+  tags: ['autodocs']
+};
+
+export const ShowCase = {
+  parameters: {
+    backgrounds: {
+      default: 'dark',
+      values: [
+        { name: 'dark', value: '#282c34' },
+        { name: 'light', value: '#fff' },
+      ],
+    },
+  },
+  render: () => (
+    <LiveProvider code={StorybookUtils.getThemeCode('dcx-highlight', style)} disabled={true} language="css">
+      <LiveEditor className="liveEditor" aria-label="editor" />
+    </LiveProvider>
+  )
+};
+
+
+export const Default = {
+  name: 'Default',
+  parameters: {
+    backgrounds: {
+      default: 'dark',
+      values: [
+        { name: 'dark', value: '#282c34' },
+        { name: 'light', value: '#fff' },
+      ],
+    },
+  },
+  args: {
+    value: 'This is the content of the code snippet.',
+  },
+};
+
