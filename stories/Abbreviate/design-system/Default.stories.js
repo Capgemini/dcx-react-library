f191be417d86a5cb9b1ddeedc84c3d5ea5841192
jgonza16 <93665374+jgonza16@users.noreply.github.com>
2023-11-17 12:43:49 +0100

    feat: add abbreviate design system
    Closes #530

---
+++ stories/Abbreviate/design-system/Default.stories.js
@@ -0,0 +1,33 @@
+import { Abbreviate } from '../../../src/abbreviate/Abbreviate';
+import { Paragraph } from '../../../src/paragraph/Paragraph';
+/**
+ * Here we display the component in its natural abbreviate, importing only the base Design System styles.
+ */
+export default {
+  title: 'DCXLibrary/Typography/Abbreviate/Design system/Default',
+  component: Abbreviate,
+  decorators: [
+    getStory => {
+      require('../../../dist/design-system/index.css');
+      return getStory();
+    },
+  ],
+  parameters: {
+    options: { showPanel: true },
+    actions: { disable: true },
+  },
+  tags: ['autodocs'],
+};
+
+export const Default = {
+  name: 'Default',
+  args: {
+    title: 'As soon as possible',
+    value: 'ASAP',
+  },
+  render: ({ title, value }) => (
+    <Paragraph>
+      Sorry, i call you <Abbreviate {...{ title, value }} />
+    </Paragraph>
+  ),
+};
