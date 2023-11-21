fafe140690c02a21d9684e2c664ba343e24beeaa
Yasemin <74928622+ybayraktutan@users.noreply.github.com>
2023-11-21 06:34:12 +0000

    Code snippet - design system (#543)
    * style: code snippet design system added
    
    * style: class name updated
    
    * style: classname fixed
    
    * style:dark theme added
    
    * chore: deleted unused code
    
    ---------
    
    Co-authored-by: ybayrakt <ybayrakt@adcs-MBP.cust.communityfibre.co.uk>
    Co-authored-by: Daniele Zurico <daniele.zurico@gmail.com>

---
+++ stories/CodeSnippet/design-system/MaterialTheme.stories.js
@@ -0,0 +1,54 @@
+import { CodeSnippet } from '../../../src/codesnippet/CodeSnippet';
+import { LiveProvider, LiveEditor } from 'react-live';
+// eslint-disable-next-line import/no-webpack-loader-syntax
+import style from '!raw-loader!../../themes/material.theme.css';
+import { StorybookUtils } from '../../../core/storybook/StorybookUtils';
+
+/**
+ * This a theme showcases an appearance similar to Material UI can be achieved.
+ * If you copy paste this snippet inside your css file you'll get a material design style
+ */
+export default {
+  title: 'DCXLibrary/Typography/CodeSnippet/Design system/Material',
+  component: CodeSnippet,
+  decorators: [
+    (getStory) => {
+      require('../../../dist/design-system/index.css');
+      require('../../themes/material.theme.css');
+      return getStory();
+    },
+  ],
+  parameters: {
+    options: { showPanel: true },
+    actions: { disable: true },
+  },
+  tags: ['autodocs'],
+};
+
+export const ShowCase = {
+  parameters: {
+    backgrounds: {
+      default: 'dark',
+      values: [
+        { name: 'dark', value: '#282c34' },
+        { name: 'light', value: '#fff' },
+      ],
+    },
+  },
+  render: () => (
+    <LiveProvider
+      code={StorybookUtils.getThemeCode('dcx-code', style)}
+      disabled={true}
+      language="css"
+    >
+      <LiveEditor className="liveEditor" aria-label="editor" />
+    </LiveProvider>
+  ),
+};
+
+export const Default = {
+  name: 'Default',
+  args: {
+    value: 'This is the content of the code snippet.',
+  },
+};
