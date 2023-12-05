58a0456013bc0bbe1ba826a1e2bee344330bb41c
Berhane Yohannes <berhane.yohannes@capgemini.com>
2023-12-05 10:27:31 +0000

    update accordion examples

--- example/src/components/AccordionDemo.tsx
+++ example/src/components/AccordionDemo.tsx
@@ -5,36 +5,77 @@ import {
   AccordionTitle,
   AccordionDetails,
 } from '@capgeminiuk/dcx-react-library';
-import './accordion.css';
+import './accordion.scss';
 
 export const AccordionDemo = () => {
   return (
-    <Accordion>
-      <AccordionItem>
-        <AccordionTitle id='1' className="accordion">
-          <>Section 1</>
-        </AccordionTitle>
-        <AccordionDetails id='1' className="panel">
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat.
-          </p>
-        </AccordionDetails>
-      </AccordionItem>
-      <AccordionItem>
-        <AccordionTitle id='2' className="accordion">
-          <>Section 2</>
-        </AccordionTitle>
-        <AccordionDetails id='2' className="panel">
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam.
-          </p>
-        </AccordionDetails>
-      </AccordionItem>
-    </Accordion>
+    <>
+      <h1>Accordion with multiple open false</h1>
+      <Accordion>
+        <AccordionItem>
+          <AccordionTitle id="1" className="accordion">
+            <>Section 1</>
+          </AccordionTitle>
+          <AccordionDetails id="1" className="panel">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </p>
+          </AccordionDetails>
+        </AccordionItem>
+        <AccordionItem>
+          <AccordionTitle id="2" className="accordion">
+            <>Section 2</>
+          </AccordionTitle>
+          <AccordionDetails id="2" className="panel">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam.
+            </p>
+          </AccordionDetails>
+        </AccordionItem>
+      </Accordion>
+      <h1>Accordion with multiple open true</h1>
+      <Accordion multipleOpen>
+        <AccordionItem>
+          <AccordionTitle id="1" className="accordion">
+            <>Title 1</>
+          </AccordionTitle>
+          <AccordionDetails id="1" className="panel">
+            <p>Panel 1 Details</p>
+          </AccordionDetails>
+        </AccordionItem>
+        <AccordionItem>
+          <AccordionTitle id="2" className="accordion">
+            <>Title 2</>
+          </AccordionTitle>
+          <AccordionDetails id="2" className="panel">
+            <p>Panel 2 Details</p>
+          </AccordionDetails>
+        </AccordionItem>
+      </Accordion>
+      <h1>Accordion with the second accordion item expanded by default</h1>
+      <Accordion expanded={['2']}>
+        <AccordionItem>
+          <AccordionTitle id="1" className="accordion">
+            <>Section 1</>
+          </AccordionTitle>
+          <AccordionDetails id="1" className="panel">
+            <p>Section 1 Details</p>
+          </AccordionDetails>
+        </AccordionItem>
+        <AccordionItem>
+          <AccordionTitle id="2" className="accordion">
+            <>Section 2</>
+          </AccordionTitle>
+          <AccordionDetails id="2" className="panel">
+            <p>Section 2 Details</p>
+          </AccordionDetails>
+        </AccordionItem>
+      </Accordion>
+    </>
   );
 };
