fa2764d96d22d67a9daca95591607e6eba063c45
Zurico.Daniele_DEFRA <daniele.zurico@gmail.com>
2024-02-05 12:50:56 +0000

    fix: linting

--- src/formCheckbox/FormCheckbox.tsx
+++ src/formCheckbox/FormCheckbox.tsx
@@ -24,7 +24,7 @@ export const FormCheckbox = ({
   inputClassName,
   labelClassName,
   itemClassName,
-  isError
+  isError,
 }: FormRadioCheckboxProps & {
   onChange?: (event: React.ChangeEvent, conditional?: string) => void;
   isError?: boolean;
@@ -33,41 +33,41 @@ export const FormCheckbox = ({
     itemClassName,
     { 'dcx-checkbox-container--error': isError },
   ]);
-  
+
   const checkboxClasses = classNames([
     inputClassName,
     { 'dcx-checkbox-checkbox--error': isError },
   ]);
-  
+
   const labelClasses = classNames([
     labelClassName,
     { 'dcx-checkbox-label--error': isError },
   ]);
-  
-   return (
-      <CheckboxRadioBase
-    type="checkbox"
-    id={id}
-    role={Roles.formCheckbox}
-    name={name}
-    value={value}
-    label={label}
-    inputProps={inputProps}
-    itemProps={itemProps}
-    labelProps={labelProps}
-    ariaLabel={ariaLabel || name}
-    ariaDataControls={ariaDataControls}
-    ariaDescribedBy={ariaDescribedBy}
-    ariaLabelledBy={ariaLabelledBy || labelProps?.id}
-    onChange={onChange}
-    conditional={conditional}
-    disabled={disabled}
-    selected={selected}
-    hint={hint}
-    nested={nested}
-    itemClassName={containerClasses}
-    inputClassName={checkboxClasses}
-    labelClassName={labelClasses}
-      />
-    )
-  }
+
+  return (
+    <CheckboxRadioBase
+      type="checkbox"
+      id={id}
+      role={Roles.formCheckbox}
+      name={name}
+      value={value}
+      label={label}
+      inputProps={inputProps}
+      itemProps={itemProps}
+      labelProps={labelProps}
+      ariaLabel={ariaLabel || name}
+      ariaDataControls={ariaDataControls}
+      ariaDescribedBy={ariaDescribedBy}
+      ariaLabelledBy={ariaLabelledBy || labelProps?.id}
+      onChange={onChange}
+      conditional={conditional}
+      disabled={disabled}
+      selected={selected}
+      hint={hint}
+      nested={nested}
+      itemClassName={containerClasses}
+      inputClassName={checkboxClasses}
+      labelClassName={labelClasses}
+    />
+  );
+};
