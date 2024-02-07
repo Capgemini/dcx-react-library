8343c36d657cc0bd3821b6d2201d692aed2e5613
Alaa Eddine <alaa.elmoudden@capgemini.com>
2024-02-07 16:35:17 +0100

    [WIP] adds changes before rebase

---
+++ src/card/__test__/Card.test.tsx
@@ -0,0 +1,106 @@
+import React from 'react';
+import { Card } from '../Card';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { CardHeader } from '../CardHeader';
+import { CardContent } from '../CardContent';
+import { CardImage } from '../CardImage';
+import { CardFooter } from '../CardFooter';
+import { CardActions } from '../CardActions';
+
+describe('Card Component', () => {
+  const dummyChild = <div>dummy card child</div>;
+
+  it('renders without errors', () => {
+    const { getByText } = render(<Card>{dummyChild}</Card>);
+
+    expect(getByText('dummy card child')).toBeInTheDocument();
+  });
+
+  it('should apply the passed className', () => {
+    const { getByText } = render(
+      <Card className="my-card-class">
+        <>dummy text</>
+      </Card>
+    );
+
+    expect(getByText('dummy text')).toBeInTheDocument();
+    expect(getByText('dummy text')).toHaveClass('my-card-class');
+  });
+
+  it('should pass props', () => {
+    render(
+      <Card data-testid="extra-prop">
+        <>dummy text</>
+      </Card>
+    );
+    expect(screen.getByTestId('extra-prop')).toBeInTheDocument();
+  });
+
+  it('renders provided children', () => {
+    render(
+      <Card data-testid="card-test-id">
+        <CardHeader data-testid="child-test-id" className="dcx-card-header">
+          <h4>dummy card header</h4>
+        </CardHeader>
+        <CardImage
+          data-testid="child-test-id"
+          className="dcx-card-header"
+          src="dummy.jpg"
+          alt="alternative"
+        />
+        <CardContent data-testid="child-test-id" className="dcx-card-content">
+          <p>dummy content</p>
+        </CardContent>
+        <CardActions data-testid="child-test-id" className="dcx-card-actions">
+          <button>See more</button>
+        </CardActions>
+        <CardFooter data-testid="child-test-id" className="dcx-card-footer">
+          <div>dummy footer</div>
+        </CardFooter>
+      </Card>
+    );
+
+    const CardComponent = screen.getByTestId('card-test-id');
+    expect(CardComponent).toBeInTheDocument();
+  });
+});
+
+// describe('CardHeader', () => {
+//   const dummyChild = <h4>header text</h4>;
+
+//   it('should render without errors', () => {
+//     const { container } = render(
+//       <CardHeader className="dcx-card-header">{dummyChild}</CardHeader>
+//     );
+//     expect(container.querySelector('dcx-card-header')).toBeInTheDocument();
+//   });
+
+//   it('should render children', () => {
+//     render(<CardHeader className="my-custom-class">{dummyChild}</CardHeader>);
+//     const childElement = screen.getByText('dummy header');
+//     expect(childElement).toBeInTheDocument();
+//   });
+
+//   it('should apply a custom className', () => {
+//     const { container } = render(
+//       <CardHeader className="my-custom-class">{dummyChild}</CardHeader>
+//     );
+//     expect(container.querySelector('my-custom-class')).toBeInTheDocument();
+//   });
+
+//   it('should pass additional props', () => {
+//     const additionalProps = {
+//       'data-testid': 'custom-test-id',
+//       'aria-label': 'custom Label',
+//     };
+
+//     const { container } = render(
+//       <CardHeader className="my-custom-class" {...additionalProps}>
+//         {dummyChild}
+//       </CardHeader>
+//     );
+//     expect(container.querySelector('my-custom-class')).toBeInTheDocument();
+//     expect(container).toHaveAttribute('aria-label', 'custom-label');
+//   });
+// });
