3d61a061e3c7c2f255535c1250e4f389394a01b0
Alaa Eddine <alaa.elmoudden@capgemini.com>
2024-02-08 16:27:25 +0100

    [wip] adds varian & layout props to card component

--- src/card/Card.tsx
+++ src/card/Card.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { classNames } from '../common';
 
-type CardProps = {
+export interface CardProps {
   /**
    * allows to pass a child or children to the component
    */
@@ -14,11 +14,39 @@ type CardProps = {
    * additional properties to support something else that we didn't plan
    */
   props?: React.HTMLAttributes<HTMLElement>;
-};
+  /**
+   * allows to align the card horizontally or vertically
+   */
+  layout?: 'horizontal' | 'vertical';
+  /**
+   * When the variant `interact` is specified the design system will provide style and token
+   * on different interactions like hover, selected, etc
+   */
+  variant?: 'default' | 'interact';
+  /**
+   * allows to control the current selected card
+   */
+  selected?: boolean;
+}
 
-export const Card = ({ children, className, ...props }: CardProps) => {
+export const Card = ({
+  children,
+  className,
+  variant = 'default',
+  layout,
+  selected,
+  ...props
+}: CardProps) => {
   return (
-    <article className={classNames(['dcx-card', className])} {...props}>
+    <article
+      className={classNames([
+        'dcx-card',
+        className,
+        layout,
+        { 'dcx-card--selected': selected },
+      ])}
+      {...props}
+    >
       {children}
     </article>
   );
