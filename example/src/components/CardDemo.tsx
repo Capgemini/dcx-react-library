bfff62281ab5eb3830d949a933626a5c6c6298b5
Alaa Eddine <alaa.elmoudden@capgemini.com>
2024-02-09 11:06:50 +0100

    adds Vertical CardDemo example

--- example/src/components/CardDemo.tsx
+++ example/src/components/CardDemo.tsx
@@ -17,30 +17,72 @@ import './cardDemo.scss';
 
 export const CardDemo = () => {
   return (
-    <Card layout="horizontal" className="card-demo">
-      <CardImage
-        className="card-demo_image"
-        src="https://placehold.co/200"
-        alt="card demo image"
-      />
-      <div className="text-wrapper">
-        <CardHeader className="card-demo_header">
-          <Heading className="card-demo_heading" level="h4" label="Order ID" />
-        </CardHeader>
-        <CardContent className="card-demo_content">
-          <List type={TYPE_LIST.UNORDERED} className="card-demo_content-list">
-            {lisItems.map((i) => (
-              <ListItem key={i.id}>{i.name}</ListItem>
-            ))}
-          </List>
-        </CardContent>
-        <CardActions className="card-demo_actions">
-          <Button type={BUTTON_TYPE.BUTTON}>
-            <FontAwesomeIcon icon={faAngleRight} />
-          </Button>
-        </CardActions>
-      </div>
-    </Card>
+    <div className="demo-cards-container">
+      <h1>Horizontal card example</h1>
+      <Card layout="horizontal" className="horizontal-card-demo">
+        <CardImage
+          className="horizontal-card_image"
+          src="https://placehold.co/200"
+          alt="card demo image"
+        />
+        <div className="text-wrapper">
+          <CardHeader className="horizontal-card_header">
+            <Heading
+              className="horizontal-card_heading"
+              level="h4"
+              label="Order ID"
+            />
+          </CardHeader>
+          <CardContent className="horizontal-card_content">
+            <List
+              type={TYPE_LIST.UNORDERED}
+              className="horizontal-card_content-list"
+            >
+              {lisItems.map((i) => (
+                <ListItem key={i.id}>{i.name}</ListItem>
+              ))}
+            </List>
+          </CardContent>
+          <CardActions className="horizontal-card_actions">
+            <Button type={BUTTON_TYPE.BUTTON}>
+              <FontAwesomeIcon icon={faAngleRight} />
+            </Button>
+          </CardActions>
+        </div>
+      </Card>
+      <h1>Vertical card example</h1>
+      <Card layout="vertical" className="vertical-card-demo">
+        <CardImage
+          className="vertical-card_image"
+          src="https://placehold.co/200"
+          alt="card demo image"
+        />
+        <div className="text-wrapper">
+          <CardHeader className="vertical-card_header">
+            <Heading
+              className="vertical-card_heading"
+              level="h4"
+              label="Order ID"
+            />
+          </CardHeader>
+          <CardContent className="vertical-card_content">
+            <List
+              type={TYPE_LIST.UNORDERED}
+              className="vertical-card_content-list"
+            >
+              {lisItems.map((i) => (
+                <ListItem key={i.id}>{i.name}</ListItem>
+              ))}
+            </List>
+          </CardContent>
+          <CardActions className="vertical-card_actions">
+            <Button type={BUTTON_TYPE.BUTTON}>
+              <FontAwesomeIcon icon={faAngleRight} />
+            </Button>
+          </CardActions>
+        </div>
+      </Card>
+    </div>
   );
 };
 
