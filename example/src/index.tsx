2e08a9b652906dd0be3feb268ac2163c8c2aba93
Zurico.Daniele_DEFRA <daniele.zurico@gmail.com>
2024-02-12 12:28:36 +0000

    feat: remove cra and migrate to vite

--- example/src/index.tsx
+++ example/src/index.tsx
@@ -1,4 +1,3 @@
-import 'react-app-polyfill/ie11';
 import reportWebVitals from './reportWebVitals';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
