2a41408320bedfeb7427004d5843f94533061d0f
Alaa Eddine <alaa.elmoudden@capgemini.com>
2024-02-27 15:13:28 +0100

    [wip] adds changes requested by first review

--- stories/Card/design-system/Default.stories.js
+++ stories/Card/design-system/Default.stories.js
@@ -6,6 +6,7 @@ import {
   CardImage,
 } from '../../../src/card';
 import { Card } from '../../../src/card/Card';
+import { Button } from '@capgeminiuk/dcx-react-library';
 
 /**
  * Here we display the component in its natural form, importing only the base Design System styles.
@@ -32,20 +33,16 @@ export const Default = {
     variant: 'default',
     children: [
       <>
-        <CardHeader className="card__header">John Doe</CardHeader>
-        <CardImage
-          className="card__image"
-          src="https://placehold.co/100"
-          alt=""
-        />
-        <CardContent className="card__content">
+        <CardHeader>John Doe</CardHeader>
+        <CardImage src="https://placehold.co/100" alt="" />
+        <CardContent>
           Nullam sodales semper ipsum, et luctus lacus sodales in. Nulla nibh
           nisl, egestas et elit et, interdum cursus massa.
         </CardContent>
-        <CardActions className="card__actions">
-          <button>Share</button>
+        <CardActions>
+          <Button label="Share" />
         </CardActions>
-        <CardFooter className="card__footer">12/02/2024</CardFooter>
+        <CardFooter>12/02/2024</CardFooter>
       </>,
     ],
   },
