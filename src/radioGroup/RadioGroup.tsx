78b46ae41d0e3389da5fc74017f0d7a7ca6c6cc3
Aranzazu Vazquez <aranzazu.vazquez-moreno@capgemini.com>
2024-03-22 13:38:03 +0100

    feat: add class host element radio-button-group

--- src/radioGroup/RadioGroup.tsx
+++ src/radioGroup/RadioGroup.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { FormRadioCheckboxBaseProps } from '../common/components/commonTypes';
 import { FormRadioCheckboxBase } from '../common/components/FormRadioCheckboxBase';
+import { classNames } from '../common/utils';
 
 export const RadioGroup = ({
   name,
@@ -20,27 +21,40 @@ export const RadioGroup = ({
   labelProps,
   legend,
   onChange,
-}: Omit<FormRadioCheckboxBaseProps, 'type'>) => (
-  <FormRadioCheckboxBase
-    {...{
-      type: 'radio',
-      name,
-      items,
-      ariaDescribedBy,
-      groupClasses,
-      error,
-      fieldsetClasses,
-      hint,
-      id,
-      inputProps,
-      itemProps,
-      itemsClasses,
-      inputClassName,
-      itemClassName,
-      labelClassName,
-      labelProps,
-      legend,
-      onChange,
-    }}
-  />
-);
+}: Omit<FormRadioCheckboxBaseProps, 'type'>) => {
+  const containerClasses = classNames([
+    groupClasses,
+    { 'dcx-radio-button-group': true },
+    { 'dcx-radio-button-group--error': !!error },
+  ]);
+
+  const containerRadioButtonsClasses = classNames([
+    itemsClasses,
+    'dcx-radio-button-group-radios',
+  ]);
+
+  return (
+    <FormRadioCheckboxBase
+      {...{
+        type: 'radio',
+        name,
+        items,
+        ariaDescribedBy,
+        groupClasses: containerClasses,
+        error,
+        fieldsetClasses,
+        hint: hint,
+        id,
+        inputProps,
+        itemProps,
+        itemsClasses: containerRadioButtonsClasses,
+        inputClassName,
+        itemClassName,
+        labelClassName,
+        labelProps,
+        legend,
+        onChange,
+      }}
+    />
+  );
+};
