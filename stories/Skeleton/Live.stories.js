331c7260a653582a0c8fa94a5aa816cb24aec2b8
Leandro Luis <l.luisbispo@outlook.com>
2024-06-20 10:13:37 +0200

    feat: skeleton - update story functionalities

---
+++ stories/Skeleton/Live.stories.js
@@ -0,0 +1,23 @@
+import { Skeleton } from '../../src/skeleton/Skeleton';
+import SkeletonLive from '../liveEdit/SkeletonLive';
+
+export default {
+  title: 'DCXLibrary/Typography/Skeleton/Live',
+  component: Skeleton,
+
+  parameters: {
+    options: {
+      showPanel: false,
+    },
+    viewMode: 'docs',
+    previewTabs: {
+      canvas: {
+        hidden: true,
+      },
+    },
+  },
+};
+
+export const Live = {
+  render: () => <SkeletonLive />,
+};
---
+++ stories/Skeleton/UnStyled.stories.js
@@ -0,0 +1,23 @@
+import { Skeleton } from '../../src/skeleton/Skeleton';
+
+export default {
+  title: 'DCXLibrary/Typography/Skeleton/Without style',
+  component: Skeleton,
+  parameters: {
+    options: {
+      showPanel: true,
+    },
+  },
+};
+
+export const Unstyled = {
+  render: function (args) {
+    return <Skeleton {...args} />;
+  },
+  args: {
+    variant: 'rectangular',
+    width: '250px',
+    height: '40px',
+    animation: 'wave',
+  },
+};
