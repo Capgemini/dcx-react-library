ec441a6a271a387c92c1b7533e5d212ee3c20bb0
Ali Jahankah <aral2@yahoo.com>
2024-08-13 16:49:10 +0100

    feat: create a basic version paginator demo component

---
+++ example/src/components/PaginatorDemo.tsx
@@ -0,0 +1,18 @@
+import React, { ReactElement } from 'react';
+
+import { Paginator } from '@capgeminiuk/dcx-react-library';
+
+export const PaginatorDemo: React.FC<ReactElement> = () => {
+  return (
+    <Paginator
+      currentPage={{ page: 3, className: 'current-page' }}
+      nextButton={{ text: 'Next >', className: 'next-button' }}
+      previousButton={{ text: '< Previous', className: 'previous-button' }}
+      totalPages={10}
+      paginatorClassName="paginator-container"
+      pageNumbersClassName="buttons"
+    />
+  );
+};
+
+export default PaginatorDemo;
