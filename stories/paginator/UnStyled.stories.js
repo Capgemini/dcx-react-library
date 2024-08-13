e49be59b21718dc46d8a7b1422a86ca8c863be64
Ali Jahankah <aral2@yahoo.com>
2024-08-13 16:59:30 +0100

    feat: add unStyled story for basic paginator

---
+++ stories/paginator/UnStyled.stories.js
@@ -0,0 +1,33 @@
+import { Paginator } from '../../src/paginator/Paginator';
+
+export default {
+  title: 'DCXLibrary/Layout/Paginator/Without style',
+  component: Paginator,
+  parameters: {
+    options: {
+      showPanel: true,
+    },
+  },
+};
+
+/**
+ * For a list of all the possible usage please look the Class based folder
+ */
+export const Unstyled = {
+  args: {
+    currentPage: {
+      page: 10,
+      className: '',
+    },
+    totalPages: 20,
+    previousButton: {
+      text: '< Previous ',
+      className: '',
+    },
+    nextButton: {
+      text: 'Next >',
+      className: '',
+    },
+    pageNumbersClassName: 'buttons',
+  },
+};
