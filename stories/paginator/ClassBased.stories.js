ce448b4b4bfed3e6c101157cd80cee7fa68e725d
Ali Jahankah <aral2@yahoo.com>
2024-09-17 17:10:59 +0100

    feat: add description

--- stories/paginator/ClassBased.stories.js
+++ stories/paginator/ClassBased.stories.js
@@ -1,7 +1,9 @@
 import './style.css';
 
 import { Paginator } from '../../src/paginator/Paginator';
-
+/**
+ * In this section we're using the Paginator component passing the relative className. Feel free to use your own css to style it as you prefer.
+ */
 export default {
   title: 'DCXLibrary/Layout/Paginator/Class based',
   component: Paginator,
