514586cef37049f354ce65ac497fab83061c99fa
Ali Jahankah <aral2@yahoo.com>
2024-09-18 12:36:33 +0100

    fix: remove unused function and edit buttons text

--- example/src/components/PaginatorDemo.tsx
+++ example/src/components/PaginatorDemo.tsx
@@ -3,14 +3,6 @@ import { Paginator } from '@capgeminiuk/dcx-react-library';
 import './paginator.scss';
 export const PaginatorDemo = () => {
   const [currentPage, setCurrentPage] = React.useState<number>(1);
-  const pages: number[] = Array.from(Array(25).keys());
-
-  const renderPageItems = () => {
-    const itemsToShow = pages.slice((currentPage - 1) * 5, currentPage * 5);
-    return itemsToShow.map((item: number, index: number) => (
-      <div key={index}>{item}</div>
-    ));
-  };
 
   return (
     <Paginator
@@ -18,9 +10,9 @@ export const PaginatorDemo = () => {
       currentPage={5}
       currentPageClassName="current-page"
       totalPages={10}
-      previousButton={<>Prev</>}
+      previousButton={<>&laquo;</>}
       previousButtonClassName="previous-button"
-      nextButton={<>Next</>}
+      nextButton={<>&raquo;</>}
       nextButtonClassName="next-button"
       pageNumbersClassName="buttons"
       onPageChange={(page) => setCurrentPage(page)}
